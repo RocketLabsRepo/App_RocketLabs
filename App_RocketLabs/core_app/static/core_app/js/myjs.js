c534104ae1ae62523364b8e5dbd325f044561c53
Osvani Colina <osvacolijim@gmail.com>
2017-08-11 17:07:12 -0400

    Cambiada imagen del header

--- App_RocketLabs/core_app/static/core_app/js/myjs.js
+++ App_RocketLabs/core_app/static/core_app/js/myjs.js
@@ -22,24 +22,3 @@ function w3_open() {
 function w3_close() {
     mySidebar.style.display = "none";
 }
-
-// Script para añadir un efecto de fadeIn al texto de 'about'
-$(document).ready(function(){
-
-  $(window).scroll(function () {
-    /* Check the location of each desired element to fade in */
-    $('.fadeInOnScroll').each( function(i){
-        
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-        
-        /* If the object is completely visible in the window, fade it it */
-        if( bottom_of_window > bottom_of_object ){
-            
-            $(this).animate({'opacity':'1'},800);
-                
-        }
-        
-    }); 
-  });
-});
