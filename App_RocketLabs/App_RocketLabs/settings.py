e823590e5db8ea87db45a700b4f271c957d2058f
Rafael Vasquez <ravb2013@gmail.com>
2017-08-24 21:06:06 -0400

    Mejorar el readme con requerimientos y configurar la DB con python-decouple.

--- App_RocketLabs/App_RocketLabs/settings.py
+++ App_RocketLabs/App_RocketLabs/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'App_RocketLabs.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST':config('DB_HOST'), 
+        'PORT':''
     }
 }
 
--- README.txt
+++ README.txt
@@ -14,7 +14,21 @@ Osvani Colina
 Rafael Vasquez
 Rabindranath Ferreira
 
-
+Requerimientos:
+	Python==2.7.12
+		# Lenguaje de programación
+	Django==1.11.4
+		# Framework web.
+	olefile==0.44
+		# Dependencia de Pillow
+	Pillow==4.2.1
+		# Libreria de utilidades para manejar ImageFields en Django.
+	psycopg2==2.7.1
+		# Libreria API para DB PostgreSQL(v9.6.3) , necesaria para trabajar desde python.
+	pytz==2017.2
+		# Definiciones de Usos Horarios mundiales para Python.
+	selenium==3.5.0
+		# Aplicación para usar Webdriver y realizar tests funcionales en Firefox.
 
 
 
