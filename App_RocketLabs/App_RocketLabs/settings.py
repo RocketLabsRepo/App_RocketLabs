6491f2a78261b55a393ff9ebf374a59fcf9054fe
Rafael Vasquez <ravb2013@gmail.com>
2017-08-29 12:08:54 -0400

    Añadir configuración de almacenar media e ignorar con .gitignore

--- App_RocketLabs/App_RocketLabs/settings.py
+++ App_RocketLabs/App_RocketLabs/settings.py
@@ -127,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configuracion de la carpeta raiz de media en la que se guardaran los
+# archivos subidos por los usuarios(Fotos, screenshots, etc)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media' )
+MEDIA_URL = '/media/'
