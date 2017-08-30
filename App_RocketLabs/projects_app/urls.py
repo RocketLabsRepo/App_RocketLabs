1b3225f0738d0595552dfd1c9153ac4c449e701e
Rafael Vasquez <ravb2013@gmail.com>
2017-08-29 22:25:59 -0400

    Bug resuelto: Desplegar imagenes de screenshots de projectos

--- App_RocketLabs/projects_app/urls.py
+++ App_RocketLabs/projects_app/urls.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 necessary for django string usage -*-
 from __future__ import unicode_literals
 
+from django.views.static import serve
+from django.conf import settings
 from django.conf.urls import url
 from . import views
 
@@ -9,3 +11,7 @@ urlpatterns = [
 	url(r'^$', views.all_projects, name='all_completed_projects'),
 	url(r'^id$', views.project_details, name='project_details')
 ]
+
+urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT,}),
+    ]
