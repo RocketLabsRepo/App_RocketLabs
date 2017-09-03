41a335a39654ba1cd8b88bd383cfd872804f889f
Osvani <osvacolijim@gmail.com>
2017-09-03 10:10:04 -0400

    Corregido bug de ver perfil de teammember sin imagen de perfil, añadida funcionalidad de loguear con facebook (Falta Generarle un codigo único a este usuario)

--- App_RocketLabs/core_app/urls.py
+++ App_RocketLabs/core_app/urls.py
@@ -6,6 +6,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.views.static import serve
 from . import views
+from django.contrib.auth import views as auth_views
+from django.conf.urls import include
 
 urlpatterns = [
 
@@ -16,6 +18,7 @@ urlpatterns = [
     url(r'^profile/$', views.profile_view, name='Edit_Profile'),
     url(r'^team/$', views.allteammember_view, name='All_Team_Members'),
     url(r'^team/([0-9]+)/$', views.detailsteammember_view, name='Details_Team_Member'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
 
 # URL para servir las imagenes de forma local durante debug.
