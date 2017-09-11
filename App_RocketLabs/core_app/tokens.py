2e18e4f3628378f6914901b2c72beee6700cc9d4
Osvani <osvacolijim@gmail.com>
2017-09-11 16:55:49 -0400

    Iniciando funcionalidad de desbloquear usuario

---
+++ App_RocketLabs/core_app/tokens.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils import six
+
+class UnlockAccountTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (
+            six.text_type(user.pk) + six.text_type(timestamp) +
+            six.text_type(user.profile.is_blocked)
+        )
+
+unlock_account_token = UnlockAccountTokenGenerator()
--- App_RocketLabs/core_app/views.py
+++ App_RocketLabs/core_app/views.py
@@ -13,7 +13,10 @@ from decouple import config
 import core_app.forms as core_forms
 from core_app.models import Skill, Profile
 from projects_app.models import Project
-
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
+from .tokens import unlock_account_token
+from django.views import View
 # Create your views here.
 
 # Helper function: Yields a generator with objects in l grouped in groups of n.
@@ -255,3 +258,22 @@ Aqui lo tienes:""" + str(user.profile.secret_link) ,
 	else:
 		form = core_forms.RecoverSecretLinkForm()
 		return render(request, 'core_app/recoversecretlink.html',{'form': form})
+
+
+class unlockaccount_view(View):
+
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and account_unlock_token.check_token(user, token):            
+			user.profile.is_blocked = False
+			login(request, user)
+			form = core_app.DefinePassForm(request.user)
+			return render(request,'core_app:restore_pass', {"form" : form })
+        else:
+            # invalid link
+            return render(request, 'registration/invalid.html')
