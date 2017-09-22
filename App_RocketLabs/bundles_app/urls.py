eeaa26cae869da472bf169c31e71f69ee0867772
Rafael Vasquez <ravb2013@gmail.com>
2017-09-21 20:29:14 -0400

    Nuevos nombres con notacion underscore.
    - Nombres de templates con notacion underscore.
    - Nuevo formato para urls (elegantes)
    - Arreglando unas fallas de diseño de la service_create_view.
    - Solo se despliegan en all_services los servicios que estan
      activos.

--- App_RocketLabs/bundles_app/urls.py
+++ App_RocketLabs/bundles_app/urls.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 necessary for django string usage -*-
 from __future__ import unicode_literals
 
-from django.views.static import serve
-from django.conf import settings
 from django.conf.urls import url
 from . import views
 
 app_name = 'bundles_app'
 
 urlpatterns = [
-    url(r'^createservice/$', views.create_service_view, name = "create_service"),
-    url(r'^services/$', views.services_view, name = "all_services"),
-    url(r'^services/([0-9]+)/$', views.detailservice_view, name = "detail_service"),
-    url(r'^editservice/([0-9]+)/$', views.editservice_view, name = "edit_service"),
+    url(r'^services/create$', views.service_create_view, name = "create_service"),
+    url(r'^services/$', views.all_services_view, name = "all_services"),
+    url(r'^services/([0-9]+)/$', views.service_detail_view, name = "detail_service"),
+    url(r'^services/([0-9]+)/edit$', views.service_edit_view, name = "edit_service"),
 ]
--- App_RocketLabs/bundles_app/views.py
+++ App_RocketLabs/bundles_app/views.py
@@ -1,45 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from bundles_app.models import Bundle, Service
-import bundles_app.forms as bundle_forms
-from  bundles_app.forms import CreateServiceForm
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, render_to_response, redirect, get_object_or_404
-from django.contrib import messages
+
+from bundles_app.models import Bundle, Service
+import bundles_app.forms as bundle_forms
+
 # Create your views here.
 
 
 #Vista para crear servicio
-def create_service_view(request):
-	form = bundle_forms.CreateServiceForm(request.POST or None)
-	if request.POST and form.is_valid():
-		form.save()
-		return HttpResponseRedirect('/')
- 	else:
- 		form = bundle_forms.CreateServiceForm()
- 		return render(request, 'bundles_app/createservice.html', {'form': form ,})
+def service_create_view(request):
+
+	if request.method == 'POST':
+		form = bundle_forms.CreateServiceForm(request.POST or None)
+		if form.is_valid():
+			form.save()
+			return redirect('bundles_app:create_service')
+		else:
+			return render(request, 'bundles_app/create_service.html', {'form': form })
+
+	form = bundle_forms.CreateServiceForm()
+	return render(request, 'bundles_app/create_service.html', {'form': form })
 
 
 
-def services_view(request):
+def all_services_view(request):
 	"""
-	Esta view le pasa a la template todos los objetos Service.
+	Esta view le pasa a la template todos los objetos Service que esten activos.
+
 	"""
-	services = Service.objects.all()
-	return render(request, 'bundles_app/services.html', {'services': services} )
+	services = Service.objects.filter(is_active=True)
+	return render(request, 'bundles_app/all_services.html', {'services': services} )
 
 
-def detailservice_view (request, service_pk):
+def service_detail_view (request, service_pk):
 	"""
 	Esta view le pasa a la template el objeto Service indicado.
+
 	"""
 	service_detail = get_object_or_404(Service, pk = service_pk)
-	return render(request, 'bundles_app/servicedetail.html', {'service_detail': service_detail})
+	return render(request, 'bundles_app/service_detail.html', {'service_detail': service_detail})
 
 
-def editservice_view(request, service_pk):
+def service_edit_view(request, service_pk):
 	if request.method == 'POST':	
 		service = get_object_or_404(Service, pk = service_pk)
 		form = bundle_forms.CreateServiceForm(request.POST, instance = service, prefix='editservice')
@@ -51,5 +57,5 @@ def editservice_view(request, service_pk):
 	else:
 		service = get_object_or_404(Service, pk = service_pk)
 		form = bundle_forms.CreateServiceForm(instance = service ,prefix='editservice')
-		return render(request, 'bundles_app/editservice.html', {'form':form} )
+		return render(request, 'bundles_app/edit_service.html', {'form':form} )
 
