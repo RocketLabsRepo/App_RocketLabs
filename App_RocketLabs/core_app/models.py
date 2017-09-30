bdd64cd374d7ee64b4f3ccc1c3821d34537fee40
Rafael Vasquez <ravb2013@gmail.com>
2017-09-30 17:46:42 -0400

    Modificación de imágenes.
    - Añadiendo un directorio para albergar las imagenes a ser usadas en el
      setup de la app.
    - Nuevas imagenes logos de skills de 500x500px. (placeholder en falta
      de algunas transparencias)
    - Nuevo avatar default con dimensiones 3x2.
    - Comentada la sección de precios mientras se remodela.
    - Eliminación de algunas imagenes avatar innecesarias.

--- App_RocketLabs/core_app/models.py
+++ App_RocketLabs/core_app/models.py
@@ -33,7 +33,7 @@ class Profile(models.Model):
 	linkedln_link = models.CharField(max_length=50, blank = True)
 	bio = models.TextField(max_length = 150, blank = True)
 	secret_link = models.CharField(max_length = 50, blank = True)
-	photo = models.ImageField(blank =True, upload_to = "photo_profile", default = "../media/photo_profile/default-user.png")
+	photo = models.ImageField(blank =True, upload_to = "photo_profile", default = "../media/photo_profile/default_user_3x2.png")
 	is_admin = models.BooleanField(default=False)
 	is_team_member= models.BooleanField(default = False)
 	failed_logins = models.SmallIntegerField(default=0)
