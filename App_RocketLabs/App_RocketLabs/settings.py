7f3b2c2bbfa28a303e9a7ae7e04f7cd89268e0bc
Osvani <osvacolijim@gmail.com>
2017-10-17 13:40:44 -0400

    Modificado DATABASES en settings

--- App_RocketLabs/App_RocketLabs/settings.py
+++ App_RocketLabs/App_RocketLabs/settings.py
@@ -121,7 +121,7 @@ DATABASES = {
     }
 }
 """
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = { 'default': dj_database_url.config() }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
