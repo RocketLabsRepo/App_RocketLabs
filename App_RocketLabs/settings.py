d0b0c04d23c11bfef5a1ae431428cf44ea344e5b
rabindranathfv <ferreiralox@gmail.com>
2017-10-18 22:36:49 -0300

    se comento confing db postgres para produccion

--- App_RocketLabs/settings.py
+++ App_RocketLabs/settings.py
@@ -109,7 +109,7 @@ AUTHENTICATION_BACKENDS = (
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-"""
+
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -120,8 +120,8 @@ DATABASES = {
         'PORT':''
     }
 }
-"""
-DATABASES = { 'default': dj_database_url.config() }
+
+#DATABASES = { 'default': dj_database_url.config() }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
