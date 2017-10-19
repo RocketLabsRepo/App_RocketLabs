b219ccc1a2388c622ae09fe80610b7531f9e2c44
Osvani <osvacolijim@gmail.com>
2017-10-18 21:03:02 -0400

    Activando DEBUG

--- App_RocketLabs/settings.py
+++ App_RocketLabs/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&@np^x6zh6ar7wn)m-)&^kg576#5&-p&wjq9b-y$^hhrdt&24n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
