e9c771d8b2caaef80f4b3262cc4f370f2cff7f6a
Osvani <osvacolijim@gmail.com>
2017-10-20 07:55:10 -0400

    FIX BUGS: Email duplicados al hacer login con redes sociales
    - Al momento de hacer login con redes sociales, si ya existe una cuenta con el email de esa red social registrado, ya no crea una cuenta con el mismo email.

--- App_RocketLabs/settings.py
+++ App_RocketLabs/settings.py
@@ -103,6 +103,24 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
+#Estas lineas son para tomar el email de facebook
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+    'fields': 'id,name,email', 
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
